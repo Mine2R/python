48a2b8e09c29a10c4cb6f8126503cbb5c6585eab
Elidnaya <romain.massa@laplateforme.io>
2024-11-28 14:13:02 +0100

    Suppression des fichiers du dossier password

--- password/aller plus loin/main.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-mot_de_passe = input("Veuillez saisir un mot de passe : ")
-hash_object = hashlib.sha256()
-hash_object.update(mot_de_passe.encode())
-mot_de_passe_hache = hash_object.hexdigest()
-print(f"Le mot de passe haché (SHA-256) est : {mot_de_passe_hache}")
